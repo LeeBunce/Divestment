a823152bf1f6316d58d213136160c0b120f39760
Lee Bunce <leebunce@hotmail.com>
2016-10-03 23:10:33 +0100

    fix tobacco typo

--- data/tidy_data.R
+++ data/tidy_data.R
@@ -11,4 +11,4 @@ march2016 <- read_csv("data/March 2016.csv")
 
 names(march2014) <- c("asset_name", "holding", "market_value", "energy", "coal", "oil_gas")
 names(march2015) <- c("asset_category", "asset_name", "holding", "market_value", "both", "oil_gas", "coal", "arms", "tobacco")
-names(march2016) <- c("asset_category", "asset_name", "holding", "market_value", "coal", "oil_gas", "either", "arms", "tobaco")
+names(march2016) <- c("asset_category", "asset_name", "holding", "market_value", "coal", "oil_gas", "either", "arms", "tobacco")
