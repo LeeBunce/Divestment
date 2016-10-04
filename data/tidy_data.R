0079226edbad94888a150f0937aa26b74602da87
Lee Bunce <leebunce@hotmail.com>
2016-10-04 16:57:07 +0100

    Improvements to tidt data script, adds date column and binds to create lpf data

--- data/tidy_data.R
+++ data/tidy_data.R
@@ -5,10 +5,28 @@ library(readr)
 library(dplyr)
 
 # Load data ---------------------------------------------------------------
-march2014 <- read_csv("data/March 2014.csv")
-march2015 <- read_csv("data/March 2015.csv")
-march2016 <- read_csv("data/March 2016.csv")
+march2014 <- read_csv("data/March 2014.csv", col_types = cols(
+  holding = "n",
+  market_value = "n"
+)) %>%
+  mutate(date = "2014-03-31")
 
-names(march2014) <- c("asset_name", "holding", "market_value", "energy", "coal", "oil_gas")
-names(march2015) <- c("asset_category", "asset_name", "holding", "market_value", "both", "oil_gas", "coal", "arms", "tobacco")
-names(march2016) <- c("asset_category", "asset_name", "holding", "market_value", "coal", "oil_gas", "either", "arms", "tobacco")
+march2015 <- read_csv("data/March 2015.csv", col_types = cols(
+  holding = "n",
+  market_value = "n"
+)) %>%
+  mutate(date = "2015-03-31")
+
+march2016 <- read_csv("data/March 2016.csv", col_types = cols(
+  holding = "n",
+  market_value = "n"
+)) %>%
+  mutate(date = "2016-03-31")
+
+# Merge data --------------------------------------------------------------
+
+lpf <- bind_rows(march2014, march2015, march2016) %>% 
+  arrange(asset_name, date)
+
+# Tidy up -----------------------------------------------------------------
+rm(march2014, march2015, march2016)
